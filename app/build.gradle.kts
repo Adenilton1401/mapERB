ec23fdb7b9518fd86a6f14404100cf8b9972a292
Adenilton1401 <adenilton1401@gmail.com>
2024-11-04 19:46:30 -0300

    adicionando as dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
