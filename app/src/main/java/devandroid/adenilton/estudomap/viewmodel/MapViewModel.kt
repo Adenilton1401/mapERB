3f717b275c828edada23293ab751ff7ad3082b7e
Adenilton1401 <adenilton1401@gmail.com>
2024-11-10 22:51:40 -0300

    Foi implementada a funcionalidade para salvar na galeria do dispositivo.

--- app/src/main/java/devandroid/adenilton/estudomap/viewmodel/MapViewModel.kt
+++ app/src/main/java/devandroid/adenilton/estudomap/viewmodel/MapViewModel.kt
@@ -1,7 +1,12 @@
 package devandroid.adenilton.estudomap.viewmodel
 
 import android.app.Application
+import android.content.ContentValues
 import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
@@ -10,6 +15,7 @@ import com.google.android.gms.maps.model.LatLng
 import devandroid.adenilton.estudomap.model.SectorPolygon
 import java.io.File
 import java.io.FileOutputStream
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -66,11 +72,7 @@ class MapViewModel(application: Application) : AndroidViewModel(application) {
 
         googleMap.snapshot { bitmap ->
             if(bitmap != null){
-                val outputStream = FileOutputStream(File(
-                    getApplication<Application>().getExternalFilesDir(null),fileName))
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-                outputStream.flush()
-                outputStream.close()
+                saveToGallery(bitmap,fileName)
                 Toast.makeText(getApplication(), "Mapa salvo como imagem!", Toast.LENGTH_SHORT).show()
             }else{
 
@@ -78,6 +80,33 @@ class MapViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
+    private fun saveToGallery(bitmap: Bitmap,fileName: String){
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
 
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                put(MediaStore.MediaColumns.IS_PENDING, 1)
+            }
+        }
+
+        val contentResolver = getApplication<Application>().contentResolver
+        val imageUri: Uri? = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+
+        imageUri?.let { uri: Uri ->
+            contentResolver.openOutputStream(uri)?.use {
+                outputStream -> bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream) }
+        }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            if(imageUri != null){
+                contentValues.clear()
+                contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0)
+                contentResolver.update(imageUri, contentValues, null,null)
+            }
+        }
+    }
 
 }
